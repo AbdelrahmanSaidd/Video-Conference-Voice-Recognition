15e37d505f3909029792c281e5b5d117758e7407
youssefamr56 <70961222+youssefamr56@users.noreply.github.com>
2024-04-28 19:57:41 +0300

    Update server.py

--- Chrome-Server/server.py
+++ Chrome-Server/server.py
@@ -1,16 +1,19 @@
 import base64
 import io
+import os
 from datetime import datetime
 
 from flask import Flask, request
-from werkzeug.utils import secure_filename
 from pydub import AudioSegment
-import os
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
+last_audio = None
+
 @app.route('/', methods=['POST'])
 def process_audio():
+    global last_audio
     # Check if the POST request has the file part
     if 'audio' not in request.form:
         print('No audio file in request.')
@@ -20,20 +23,32 @@ def process_audio():
     audio_data = base64.b64decode(request.form['audio'].split(',')[1])
     audio_file = io.BytesIO(audio_data)
 
-    timestamp = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
-    filename = secure_filename(f'audio_{timestamp}.wav')
-    file_path = os.path.join('/Users/youssefabouelenin/Downloads', filename)
-
     # Convert the audio file to .wav format in memory
     try:
         audio = AudioSegment.from_file(audio_file, format="webm")
-        audio.export(file_path, format='wav')
-        print(f'Audio file converted to .wav: {file_path}')
     except Exception as e:
         print(f'Error converting audio to .wav: {e}')
         return 'Error converting audio to .wav'
 
-    print(f'Audio file saved as .wav: {file_path}')
+    # Concatenate the last 2 seconds of the previous file with the first second of the new one
+    if last_audio is not None:
+        audio = last_audio[-2000:] + audio[:1000]
+
+    timestamp = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+    filename = secure_filename(f'audio_{timestamp}.wav')
+    file_path = os.path.join('/Users/youssefabouelenin/Downloads', filename)
+
+    # Save the audio file
+    try:
+        audio.export(file_path, format='wav')
+        print(f'Audio file saved as .wav: {file_path}')
+    except Exception as e:
+        print(f'Error saving audio file: {e}')
+        return 'Error saving audio file.'
+
+    # Update the last audio
+    last_audio = audio
+
     return 'Audio file received and saved as .wav.'
 
 
